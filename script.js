fe4f651dd28df9dd00d310a7d7c1d9dd4c6363f4
kelloggz24 <129332906+kelloggz24@users.noreply.github.com>
2024-01-17 09:54:15 +0100

    dyk,de

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 function NSI(){
     var element = document.getElementsByClassName("NSI");
     for(let i=0; i < element.length; i++){
-        element[i].bgColor="#b8c8d1";
+        if(element[i].bgColor==="#d6d6d6"){
+            element[i].bgColor="#b8c8d1";
+        }else{element[i].bgColor="#d6d6d6"}
     }
 }
 function Maths(){
     var element = document.getElementsByClassName("Maths");
     for(let i=0; i < element.length; i++){
-        element[i].bgColor="#ee7d7d";
+        if(element[i].bgColor==="#d6d6d6"){
+            element[i].bgColor="#ee7d7d";
+        }else{element[i].bgColor="#d6d6d6"}
     }
 }
 
 function PC(){
     var element = document.getElementsByClassName("PC");
     for(let i=0; i < element.length; i++){
-        element[i].bgColor="#db9ced";
+        if(element[i].bgColor==="#d6d6d6"){
+            element[i].bgColor="#db9ced";
+        }else{element[i].bgColor="#d6d6d6"}
     }
 }
