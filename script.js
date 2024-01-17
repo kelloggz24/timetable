88fe94f7be15098b8d6c942b3a6cc6d3f34afa56
kelloggz24 <129332906+kelloggz24@users.noreply.github.com>
2024-01-17 09:20:26 +0100

    JS MARCHE LA PYUTAIN DE SA MERE

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function NSI(){
-    console.log("qvguiqvgl")
     var element = document.getElementsByClassName("NSI");
     for(let i=0; i < element.length; i++){
         element[i].bgColor="#b8c8d1";
+    }
+}
+function Maths(){
+    var element = document.getElementsByClassName("NSI");
+    for(let i=0; i < element.length; i++){
+        element[i].bgColor="#ee7d7d";
+    }
+}
+
+function PC(){
+    var element = document.getElementsByClassName("NSI");
+    for(let i=0; i < element.length; i++){
+        element[i].bgColor="#db9ced";
     }
 }
