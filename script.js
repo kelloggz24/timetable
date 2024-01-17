6acd51b01efb5e0c20af5aedcd34818a19ee7960
kelloggz24 <129332906+kelloggz24@users.noreply.github.com>
2024-01-17 08:45:01 +0100

    <etnjqh

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function NSI(){
     var element = index.getElementByClassName("NSI")
     for(let i=0;i<element.length;i++){
-        index.element[i].style.backgroundColor="red"
+        element[i].style.backgroundColor="red";
     }
 }
