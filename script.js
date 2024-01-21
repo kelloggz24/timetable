afd1b453cbc5952729a53696a2034205ca6d9d2d
kelloggz24 <crusoerobin2408@gmail.com>
2024-01-21 23:52:08 +0100

    JS is perfectly working. Goodnight.

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-function NSI(){
-    var element = document.getElementsByClassName("NSI");
+function highlight(className){
+    var element = document.getElementsByClassName(className);
     for(let i=0; i < element.length; i++){
-        if(element[i].bgColor==="#d6d6d6"){
-            element[i].bgColor="#b8c8d1";
-        }else{element[i].bgColor="#d6d6d6"}
+        if(element[i].bgColor==="#ffd966"){
+            element[i].bgColor="#d6d6d6";
+        }else{element[i].bgColor="#ffd966"}
     }
 }
-function Maths(){
-    var element = document.getElementsByClassName("Maths");
-    for(let i=0; i < element.length; i++){
-        if(element[i].bgColor==="#d6d6d6"){
-            element[i].bgColor="#ee7d7d";
-        }else{element[i].bgColor="#d6d6d6"}
+const clname = ['NSI','Maths', 'PC', 'FR', 'Anglais', 'HG', 'Allemand', 'ES', 'EPS']
+for(let j=0; j < clname.length; j++){
+    var elmt = document.getElementsByClassName(clname[j]);
+        for(let i=0; i < elmt.length; i++){
+            elmt[i].addEventListener("mouseover", function(){highlight(clname[j])})
+            elmt[i].addEventListener("mouseout", function(){highlight(clname[j])})
+        }
     }
-}
 
-function PC(){
-    var element = document.getElementsByClassName("PC");
-    for(let i=0; i < element.length; i++){
-        if(element[i].bgColor==="#d6d6d6"){
-            element[i].bgColor="#db9ced";
-        }else{element[i].bgColor="#d6d6d6"}
-    }
-}
